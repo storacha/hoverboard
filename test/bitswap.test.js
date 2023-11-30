ca390b17d27fbae2f50a63f8ec47d8073c788973
bengo <171782+gobengo@users.noreply.github.com>
2023-11-29 17:35:58 -0800

    wip crafting bitswap content-claims test

--- test/bitswap.test.js
+++ test/bitswap.test.js
@@ -12,6 +12,8 @@ import { Blob } from 'node:buffer'
 import { createDynamo, createDynamoTable, createS3, createS3Bucket } from './lib/aws.js'
 import { peerId } from './fixture/peer.js'
 import test from 'ava'
+import { ShardingStream, UnixFS } from '@web3-storage/upload-client'
+import assert from 'node:assert'
 
 const workers = []
 
@@ -51,41 +53,7 @@ function getListenAddr ({ port, address }) {
  */
 test('helia bitswap', async t => {
   t.timeout(60_000)
-  const [dynamo, s3] = await Promise.all([createDynamo(), createS3()])
-  const [table, bucket] = await Promise.all([createDynamoTable(dynamo.client), createS3Bucket(s3.client)])
-  const builder = new Builder(dynamo.client, table, s3.client, 'us-west-2', bucket)
-  const encoder = new TextEncoder()
-  const expected = 'hoverboard 🛹'
-  const blob = new Blob([encoder.encode(expected)])
-  const root = await builder.add(blob)
-
-  console.log('Creating local hoverboard')
-  const worker = await createWorker({
-    S3_ENDPOINT: s3.endpoint,
-    DYNAMO_ENDPOINT: dynamo.endpoint,
-    DYNAMO_REGION: dynamo.region,
-    DYNAMO_TABLE: table,
-    AWS_ACCESS_KEY_ID: s3.credentials.accessKeyId,
-    AWS_SECRET_ACCESS_KEY: s3.credentials.secretAccessKey,
-    PEER_ID_JSON: JSON.stringify(peerId)
-  })
-  const hoverboard = getListenAddr(worker)
-
-  console.log('Creating local libp2p')
-  const libp2p = await createLibp2p({
-    connectionEncryption: [noise()],
-    transports: [webSockets()],
-    streamMuxers: [mplex()],
-    services: {
-      identify: identifyService()
-    }
-  })
-
-  console.log('Creating local helia')
-  const helia = await createHelia({
-    libp2p
-  })
-  const heliaFs = unixfs(helia)
+  const { expected, libp2p, helia, heliaFs, hoverboard, root } = await createHeliaBitswapScenario()
 
   console.log(`Dialing ${hoverboard}`)
   const peer = await libp2p.dial(hoverboard)
@@ -107,27 +75,69 @@ test('helia bitswap', async t => {
 })
 
 test('helia bitswap + content-claims', async t => {
-  const { expected, libp2p, helia, heliaFs, hoverboard, root } = await createHeliaBitswapScenario()
-  console.log(`Dialing ${hoverboard}`)
-  const peer = await libp2p.dial(hoverboard)
-  t.is(peer.remoteAddr.getPeerId().toString(), peerId.id)
+  const contentA = await createMockClaimableContent(`contentA-${Math.random().toString().slice(2)}`)
+  console.log('contentA', {
+    ...contentA,
+    text: (new TextDecoder()).decode(contentA.buffer)
+  })
+  const contentClaims = createMockContentClaims([contentA])
+  const { libp2p, heliaFs, hoverboard } = await createHeliaBitswapScenario({ contentClaims })
+  await libp2p.dial(hoverboard)
 
-  const decoder = new TextDecoder('utf8')
-  let text = ''
+  // we should be able to use helia.cat(contentA.unixfsLink) and get blocks that come form contentA.unixfsCar
 
-  console.log(`Fetching ${root}`)
-  for await (const chunk of heliaFs.cat(root)) {
-    text += decoder.decode(chunk, { stream: true })
-  }
-
-  text += decoder.decode()
-
-  t.true(await helia.blockstore.has(root), 'block should now be in helia blockstore')
-
-  t.is(text, expected, 'bitswap roundtrippin')
+  const contentACat = heliaFs.cat(contentA.unixfsLink)
+  const contentACatText = await (import('uint8arrays').then(m => m.toString(contentACat)))
+  console.log('contentACatText', contentACatText)
+  const unixfsText = await (import('uint8arrays').then(m => m.toString(contentA.unixfsCar)))
+  console.log('unixfsText', unixfsText)
 })
 
-async function createHeliaBitswapScenario () {
+/**
+ * @typedef ClaimableContent
+ * @property {ArrayBuffer} buffer
+ */
+
+function createMockContentClaims () {
+  const url = process.env.CONTENT_CLAIMS ?? 'https://staging.claims.web3.storage'
+  return {
+    url
+  }
+}
+
+async function createMockClaimableContent (text = (new Date()).toISOString()) {
+  const buffer = (new TextEncoder()).encode(text)
+  /** @type {import('multiformats').UnknownLink?} */
+  let unixfsLink
+  /** @type {ArrayBuffer} */
+  let unixfsCar
+  await UnixFS.createFileEncoderStream(new Blob(buffer))
+    .pipeThrough(new globalThis.TransformStream({
+      transform (block, controller) {
+        unixfsLink = block.cid
+        controller.enqueue(block)
+      }
+    }))
+    .pipeThrough(new ShardingStream())
+    .pipeTo(new globalThis.WritableStream({
+      write: async carBuffer => {
+        unixfsCar = carBuffer
+      }
+    }))
+  assert.ok(unixfsLink, 'unixfsLink must be a link')
+  assert.ok(unixfsCar, 'unixfsCar must be a truthy')
+  return { buffer, unixfsLink, unixfsCar }
+}
+
+/**
+ * test scenario that setups up a hoverboard running in a worker,
+ * a libp2p that can connect to it over websocket,
+ * and some example data to test with
+ * @param {object} options
+ * @param {object} [options.contentClaims] - content claims service
+ * @param {string} [options.contentClaims.url] - url of content claims service
+ */
+async function createHeliaBitswapScenario ({ contentClaims } = {}) {
   const [dynamo, s3] = await Promise.all([createDynamo(), createS3()])
   const [table, bucket] = await Promise.all([createDynamoTable(dynamo.client), createS3Bucket(s3.client)])
   const builder = new Builder(dynamo.client, table, s3.client, 'us-west-2', bucket)
@@ -136,7 +146,7 @@ async function createHeliaBitswapScenario () {
   const blob = new Blob([encoder.encode(expected)])
   const root = await builder.add(blob)
 
-  console.log('Creating local hoverboard')
+  console.warn('Creating local hoverboard')
   const worker = await createWorker({
     S3_ENDPOINT: s3.endpoint,
     DYNAMO_ENDPOINT: dynamo.endpoint,
@@ -144,11 +154,12 @@ async function createHeliaBitswapScenario () {
     DYNAMO_TABLE: table,
     AWS_ACCESS_KEY_ID: s3.credentials.accessKeyId,
     AWS_SECRET_ACCESS_KEY: s3.credentials.secretAccessKey,
-    PEER_ID_JSON: JSON.stringify(peerId)
+    PEER_ID_JSON: JSON.stringify(peerId),
+    ...(contentClaims.url ? { CONTENT_CLAIMS: contentClaims.url } : {})
   })
   const hoverboard = getListenAddr(worker)
 
-  console.log('Creating local libp2p')
+  console.warn('Creating local libp2p')
   const libp2p = await createLibp2p({
     connectionEncryption: [noise()],
     transports: [webSockets()],
@@ -158,7 +169,7 @@ async function createHeliaBitswapScenario () {
     }
   })
 
-  console.log('Creating local helia')
+  console.warn('Creating local helia')
   const helia = await createHelia({
     libp2p
   })
