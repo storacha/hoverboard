d5a3dfaceba09a7fbf87d2c154d1aa879e235921
bengo <171782+gobengo@users.noreply.github.com>
2023-12-04 13:14:11 -0800

    update @web3-storage/upload-client to latest

--- test/lib/content-claims-nodejs.js
+++ test/lib/content-claims-nodejs.js
@@ -1,8 +1,21 @@
 import { Map as LinkMap } from 'lnmap'
 import * as Link from 'multiformats/link'
 import { CARWriterStream } from 'carstream/writer'
-import { Writable } from 'stream'
+import http from 'node:http'
+import { Writable } from 'node:stream'
+import { CARReaderStream } from 'carstream'
+import * as raw from 'multiformats/codecs/raw'
+import * as Block from 'multiformats/block'
+import { sha256 } from 'multiformats/hashes/sha2'
+import { identity } from 'multiformats/hashes/identity'
+import { blake2b256 } from '@multiformats/blake2/blake2b'
+import * as pb from '@ipld/dag-pb'
+import * as cbor from '@ipld/dag-cbor'
+import * as json from '@ipld/dag-json'
+import { Assert } from '@web3-storage/content-claims/capability'
+import * as ed25519 from '@ucanto/principal/ed25519'
 
+/* global WritableStream */
 /* global ReadableStream */
 
 /**
@@ -40,4 +53,119 @@ export const mockClaimsService = async (
       .pipeTo(Writable.toWeb(res))
   }
   return { claims, setClaims, listener }
+}
+
+const Decoders = {
+  [raw.code]: raw,
+  [pb.code]: pb,
+  [cbor.code]: cbor,
+  [json.code]: json
+}
+
+const Hashers = {
+  [identity.code]: identity,
+  [sha256.code]: sha256,
+  [blake2b256.code]: blake2b256
+}
+
+/**
+ * @param {import('@ucanto/interface').Signer} signer
+ * @param {import('multiformats').UnknownLink} dataCid
+ * @param {import('cardex/api').CARLink} carCid
+ * @param {ReadableStream<Uint8Array>} carStream CAR file data
+ * @param {import('multiformats').Link} indexCid
+ * @param {import('cardex/api').CARLink} indexCarCid
+ */
+export const generateClaims = async (signer, dataCid, carCid, carStream, indexCid, indexCarCid) => {
+  /** @type {Claims} */
+  const claims = new LinkMap()
+
+  // partition claim for the data CID
+  claims.set(dataCid, [
+    await encode(Assert.partition.invoke({
+      issuer: signer,
+      audience: signer,
+      with: signer.did(),
+      nb: {
+        content: dataCid,
+        parts: [carCid]
+      }
+    }))
+  ])
+
+  /** @type {Map<import('multiformats').UnknownLink, RelationIndexData>} */
+  const indexData = new LinkMap()
+
+  await carStream
+    .pipeThrough(new CARReaderStream())
+    .pipeTo(new WritableStream({
+      async write ({ cid, bytes }) {
+        // @ts-expect-error cid.code may not be in Decoders
+        const decoder = Decoders[cid.code]
+        if (!decoder) throw Object.assign(new Error(`missing decoder: ${cid.code}`), { code: 'ERR_MISSING_DECODER' })
+
+        // @ts-expect-error cid.multihash.code may not be in Hashers
+        const hasher = Hashers[cid.multihash.code]
+        if (!hasher) throw Object.assign(new Error(`missing hasher: ${cid.multihash.code}`), { code: 'ERR_MISSING_HASHER' })
+
+        const block = await Block.decode({ bytes, codec: decoder, hasher })
+        indexData.set(cid, { cid, bytes, children: [...block.links()].map(([, cid]) => cid) })
+      }
+    }))
+
+  for (const [cid, { children }] of indexData) {
+    const invocation = Assert.relation.invoke({
+      issuer: signer,
+      audience: signer,
+      with: signer.did(),
+      nb: {
+        content: cid,
+        children,
+        parts: [{
+          content: carCid,
+          includes: {
+            content: indexCid,
+            parts: [indexCarCid]
+          }
+        }]
+      }
+    })
+
+    const blocks = claims.get(cid) ?? []
+    blocks.push(await encode(invocation))
+    claims.set(cid, blocks)
+  }
+
+  // partition claim for the index
+  claims.set(indexCid, [
+    await encode(Assert.partition.invoke({
+      issuer: signer,
+      audience: signer,
+      with: signer.did(),
+      nb: {
+        content: indexCid,
+        parts: [indexCarCid]
+      }
+    }))
+  ])
+
+  return claims
+}
+
+/**
+ * multicodec code indicating content is a CAR file
+ * @see https://github.com/multiformats/multicodec/blob/master/table.csv#L140
+ */
+const CAR_MULTICODEC_CODE = 0x0202
+
+/**
+ * Encode a claim to a block.
+ * @template {import('@ipld/dag-ucan').Capability} Capability
+ * @param {import('@ucanto/interface').IssuedInvocationView<Capability>} invocation
+ */
+const encode = async invocation => {
+  const view = await invocation.buildIPLDView()
+  const bytes = await view.archive()
+  if (bytes.error) throw new Error('failed to archive')
+  return { cid: Link.create(CAR_MULTICODEC_CODE, await sha256.digest(bytes.ok)), bytes: bytes.ok }
 }
